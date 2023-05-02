cfc11655d8c291ef5572d1984317d4e4f3f23266
Zachary Sain <zachsain@gmail.com>
2023-05-02 14:17:15 -0500

    files added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Header';
+import ReactDOM from "react-dom/client";
+import { Switch, Route } from "react-router-dom";
+import Card from './Card';
+import SwipeButtons from './SwipeButtons';
+import Chats from './Chats';
+import ChatScreen from './ChatScreen';
 
 function App() {
   return (
     <div className="App">
-     
-    Hello world 
+      {/* <Header/> */}
+      <Switch>
+      <Route path = "/chat/:person">
+          <Header backButton="/chat" /> 
+          <ChatScreen />
+        </Route>
+        <Route path = "/chat">
+          <Header backButton="/" /> 
+          <Chats />
+        </Route>
+        <Route exact path = "/">
+          <Header />
+          <Card />
+          <SwipeButtons />
+        </Route>
+      </Switch>
+      
     </div>
   );
 }
 
+
 export default App;
+
